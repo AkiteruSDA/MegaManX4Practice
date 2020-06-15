97541c8a66d084002512d3558d82804817142bbc
Akiteru <akiteru@live.com>
2020-06-15 17:21:41 -0230

    Remove indents, come up with way to jump away to another org to replace code then jump back to cave with labels

--- src/hack.asm
+++ src/hack.asm
@@ -1,6 +1,20 @@
-	.psx
-	.open "../build/SLUS_005_VANILLA.61", "../build/SLUS_005.61", 0x8000F800
+.psx
 
-	.include "stageselect.asm"
+.open "../build/SLUS_005_VANILLA.61", "../build/SLUS_005.61", 0x80010000-0x800
 
-	.close
+.macro replace,dest
+	.org dest
+.endmacro
+
+.macro endreplace,nextlabel
+	.org org(nextlabel)
+.endmacro
+
+; This block is the main area for now.
+.org 0x8011C200
+.area 0x03F0
+
+.include "stageselect.asm"
+
+.endarea
+.close
--- src/stageselect.asm
+++ src/stageselect.asm
@@ -1,13 +1,20 @@
-    .psx
+.psx
 
-    ; Go to stage select after character select.
-	.org 0x8002E4D0
-	lw r2,0x3A4(r1)
+; Go to stage select after character select.
+stage_select_character_select:
+replace 0x8002E4D0
+    ; Calling function 3 in this jump table instead of 0
+    lw v0, 0x398+3*4(at)
+endreplace stage_select_character_select
 
-    ; Automatically scroll camera down on stage select.
-    .org 0x8002E7E0
+; Automatically scroll camera down on stage select.
+stage_select_camera_scroll:
+replace 0x8002E7E0
     nop ; nop is 4 bytes in MIPS
+endreplace stage_select_camera_scroll
 
-    ; Skip maverick intros.
-    .org 0x8002EED8
+; Skip maverick intros.
+stage_select_skip_intros:
+replace 0x8002EED8
     nop
+endreplace stage_select_skip_intros
